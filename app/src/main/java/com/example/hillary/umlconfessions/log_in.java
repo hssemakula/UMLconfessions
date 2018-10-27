b35f3c0b23a37124f30f769540c68e00202c7e47
hssemakula <hillary_ssemakula@student.uml.edu>
2018-10-27 19:19:25 -0400

    2 login screens

--- app/src/main/java/com/example/hillary/umlconfessions/log_in.java
+++ app/src/main/java/com/example/hillary/umlconfessions/log_in.java
@@ -8,6 +8,6 @@ public class log_in extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_log_in);
+        setContentView(R.layout.log_in_1);
     }
 }
