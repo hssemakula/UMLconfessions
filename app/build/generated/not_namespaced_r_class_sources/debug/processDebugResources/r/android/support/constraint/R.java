ce1a06b31d7fd6b062a829947fc7bcf184b54d49
hssemakula <hillary_ssemakula@student.uml.edu>
2018-11-08 13:37:23 -0500

    drawer fixed

--- app/build/generated/not_namespaced_r_class_sources/debug/processDebugResources/r/android/support/constraint/R.java
+++ app/build/generated/not_namespaced_r_class_sources/debug/processDebugResources/r/android/support/constraint/R.java
@@ -73,20 +73,20 @@ public final class R {
     public static final class id {
         private id() {}
 
-        public static final int bottom = 0x7f080022;
-        public static final int end = 0x7f08003f;
-        public static final int gone = 0x7f08004c;
-        public static final int invisible = 0x7f080056;
-        public static final int left = 0x7f08005b;
-        public static final int packed = 0x7f080077;
-        public static final int parent = 0x7f080079;
-        public static final int percent = 0x7f08007d;
-        public static final int right = 0x7f080082;
-        public static final int spread = 0x7f0800a7;
-        public static final int spread_inside = 0x7f0800a8;
-        public static final int start = 0x7f0800ad;
-        public static final int top = 0x7f0800c3;
-        public static final int wrap = 0x7f0800d3;
+        public static final int bottom = 0x7f080024;
+        public static final int end = 0x7f080042;
+        public static final int gone = 0x7f080051;
+        public static final int invisible = 0x7f08005b;
+        public static final int left = 0x7f080060;
+        public static final int packed = 0x7f080083;
+        public static final int parent = 0x7f080085;
+        public static final int percent = 0x7f080089;
+        public static final int right = 0x7f08008e;
+        public static final int spread = 0x7f0800b3;
+        public static final int spread_inside = 0x7f0800b4;
+        public static final int start = 0x7f0800b9;
+        public static final int top = 0x7f0800d1;
+        public static final int wrap = 0x7f0800e1;
     }
     public static final class styleable {
         private styleable() {}
