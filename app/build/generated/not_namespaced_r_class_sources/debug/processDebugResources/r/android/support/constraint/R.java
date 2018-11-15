d520146d44c3d5046b7d35b3e770907c1aa7a2df
hssemakula <hillary_ssemakula@student.uml.edu>
2018-11-15 13:19:32 -0500

    example recycler view

--- app/build/generated/not_namespaced_r_class_sources/debug/processDebugResources/r/android/support/constraint/R.java
+++ app/build/generated/not_namespaced_r_class_sources/debug/processDebugResources/r/android/support/constraint/R.java
@@ -74,19 +74,19 @@ public final class R {
         private id() {}
 
         public static final int bottom = 0x7f080023;
-        public static final int end = 0x7f080045;
-        public static final int gone = 0x7f080056;
-        public static final int invisible = 0x7f080060;
-        public static final int left = 0x7f080065;
-        public static final int packed = 0x7f080081;
-        public static final int parent = 0x7f080083;
-        public static final int percent = 0x7f080087;
-        public static final int right = 0x7f08008e;
-        public static final int spread = 0x7f0800b3;
-        public static final int spread_inside = 0x7f0800b4;
-        public static final int start = 0x7f0800b9;
-        public static final int top = 0x7f0800d2;
-        public static final int wrap = 0x7f0800e4;
+        public static final int end = 0x7f08004e;
+        public static final int gone = 0x7f08005f;
+        public static final int invisible = 0x7f080069;
+        public static final int left = 0x7f08006e;
+        public static final int packed = 0x7f08008a;
+        public static final int parent = 0x7f08008c;
+        public static final int percent = 0x7f080090;
+        public static final int right = 0x7f08009a;
+        public static final int spread = 0x7f0800c0;
+        public static final int spread_inside = 0x7f0800c1;
+        public static final int start = 0x7f0800c6;
+        public static final int top = 0x7f0800e0;
+        public static final int wrap = 0x7f0800f4;
     }
     public static final class styleable {
         private styleable() {}
--- app/src/main/java/com/example/hillary/umlconfessions/feedFragment.java
+++ app/src/main/java/com/example/hillary/umlconfessions/feedFragment.java
@@ -4,15 +4,45 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.util.ArrayList;
+
 public class feedFragment extends Fragment {
+
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
+    private ArrayList<String> myDataset = new ArrayList<String>(); //bogus arraylist, data from server would probably be here
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.posts_fragment, container, false);
+
+        View currentView = inflater.inflate(R.layout.posts_fragment, container, false);
+
+        initializeRecycler((RecyclerView)currentView);
+
+        return currentView;
+    }
+
+    private void initializeRecycler(RecyclerView view){
+        mRecyclerView =  view.findViewById(R.id.posts_recycler);
+
+        // use this setting to improve performance if you know that changes
+        // in content do not change the layout size of the RecyclerView
+        mRecyclerView.setHasFixedSize(true);
+
+        // use a linear layout manager
+        mLayoutManager = new LinearLayoutManager(view.getContext());
+        mRecyclerView.setLayoutManager(mLayoutManager);
+
+        // specify an adapter (see also next example)
+        mAdapter = new MyAdapter(myDataset);
+        mRecyclerView.setAdapter(mAdapter);
     }
 }
