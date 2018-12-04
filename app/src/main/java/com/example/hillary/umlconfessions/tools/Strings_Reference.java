5683523e3bef02cbb40a6bf400261322f0030dfa
mmoschella <m_moschella@massbay.edu>
2018-12-03 21:46:48 -0500

    c

--- app/src/main/java/com/example/hillary/umlconfessions/tools/Strings_Reference.java
+++ app/src/main/java/com/example/hillary/umlconfessions/tools/Strings_Reference.java
@@ -13,6 +13,6 @@ public class Strings_Reference {
     public static final String ADD_CONFESSIONS = "confession";
     public static final String MY_CONFESSIONS = "my_confessions";
     public static final String USERINFO_RECORD = "record";
-    public static final String KEY_FOR_COMMENTS_NUMBER = "comments_number";
+    public static final String KEY_FOR_COMMENTS_NUMBER = "commentCount";
     //public static final String
 }
